5b0d58922cc3e102c484a98f49ef1bc69b1007e9
SpencerSzabados <>
2024-09-12 14:46:10 +0000

    Removed unsed config files. Began developing mlp diffusion implementation.

---
+++ model/model_modules/mlp_layers.py
@@ -0,0 +1,110 @@
+"""
+    File contains layer definitions used in constructing mlp diffusion model.
+
+    Implementation based on (https://github.com/tanelp/tiny-diffusion/blob/master/ddpm.py)
+"""
+
+
+import torch as th
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SinusoidalEmbedding(nn.Module):
+    def __init__(self, size: int, scale: float = 1.0):
+        super().__init__()
+        self.size = size
+        self.scale = scale
+
+    def forward(self, x: th.Tensor):
+        x = x * self.scale
+        half_size = self.size // 2
+        emb = th.log(th.Tensor([10000.0])) / (half_size - 1)
+        emb = th.exp(-emb * th.arange(half_size))
+        emb = x.unsqueeze(-1) * emb.unsqueeze(0).to(device=x.device)
+        emb = th.cat((th.sin(emb), th.cos(emb)), dim=-1)
+        return emb
+
+    def __len__(self):
+        return self.size
+
+
+class LinearEmbedding(nn.Module):
+    def __init__(self, size: int, scale: float = 1.0):
+        super().__init__()
+        self.size = size
+        self.scale = scale
+
+    def forward(self, x: th.Tensor):
+        x = x / self.size * self.scale
+        return x.unsqueeze(-1)
+
+    def __len__(self):
+        return 1
+
+
+class LearnableEmbedding(nn.Module):
+    def __init__(self, size: int):
+        super().__init__()
+        self.size = size
+        self.linear = nn.Linear(1, size)
+
+    def forward(self, x: th.Tensor):
+        return self.linear(x.unsqueeze(-1).float() / self.size)
+
+    def __len__(self):
+        return self.size
+
+
+class IdentityEmbedding(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x: th.Tensor):
+        return x.unsqueeze(-1)
+
+    def __len__(self):
+        return 1
+
+
+class ZeroEmbedding(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x: th.Tensor):
+        return x.unsqueeze(-1) * 0
+
+    def __len__(self):
+        return 1
+
+
+class PositionalEmbedding(nn.Module):
+    def __init__(self, size: int, type: str, **kwargs):
+        super().__init__()
+
+        if type == "sinusoidal":
+            self.layer = SinusoidalEmbedding(size, **kwargs)
+        elif type == "linear":
+            self.layer = LinearEmbedding(size, **kwargs)
+        elif type == "learnable":
+            self.layer = LearnableEmbedding(size)
+        elif type == "zero":
+            self.layer = ZeroEmbedding()
+        elif type == "identity":
+            self.layer = IdentityEmbedding()
+        else:
+            raise ValueError(f"Unknown positional embedding type: {type}")
+
+    def forward(self, x: th.Tensor):
+        return self.layer(x)
+
+
+class MLPBlock(nn.Module):
+    def __init__(self, size: int):
+        super().__init__()
+
+        self.ff = nn.Linear(size, size)
+        self.act = nn.GELU()
+
+    def forward(self, x: th.Tensor):
+        return x + self.act(self.ff(x))
